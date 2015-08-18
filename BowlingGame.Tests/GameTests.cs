7c606635d2bbf83e02aaa10eab4ae82aa61470ec
Piotr Karpala <pkarpala@infusion.com>
2015-08-18 22:54:19 +0200

    katta completed

---
+++ BowlingGame.Tests/GameTests.cs
@@ -0,0 +1,105 @@
+﻿using NUnit.Framework;
+
+namespace BowlingGame.Tests
+{
+    [TestFixture]
+    public class GameTests
+    {
+        private Game _game;
+
+        private void RollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                _game.Roll(pins);
+            }
+        }
+
+        private void RollSpare()
+        {
+            _game.Roll(5);
+            _game.Roll(5);
+        }
+
+        private void RollStrike()
+        {
+            _game.Roll(10);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _game = new Game();
+        }
+
+        [Test]
+        public void Game_Should_HaveScoreOfZero_When_NoPinsHit()
+        {
+            // Arrange
+            RollMany(20, 0);
+
+            // Act
+            var result = _game.Score();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Game_Should_HaveScore20_When_NoOnePinEveryFrame()
+        {
+            // Arrange
+            RollMany(20, 1);
+
+            // Act
+            var result = _game.Score();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(20), "Score should be 20 when one pin hit in every frame");
+        }
+
+        [Test]
+        public void Game_Should_HaveScore16_When_SpareAnd3()
+        {
+            // Arrange
+            RollSpare();
+            _game.Roll(3);
+            RollMany(17, 0);
+
+            // Act
+            var result = _game.Score();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(16));
+        }
+
+        [Test]
+        public void Game_Should_HaveScore24_WhenStrikeAnd3And4()
+        {
+            // Arrange
+            RollStrike();
+            _game.Roll(3);
+            _game.Roll(4);
+            RollMany(16, 0);
+
+            // Act
+            var result = _game.Score();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(24));
+        }
+
+        [Test]
+        public void Game_Should_HaveScore300_WhenOnlySrikes()
+        {
+            // Arrange
+            RollMany(12, 10);
+
+            // Act
+            var result = _game.Score();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(300));
+        }
+    }
+}
---
+++ BowlingGame/Game.cs
@@ -0,0 +1,67 @@
+﻿namespace BowlingGame
+{
+    public class Game
+    {
+        private const int MaxRollsInBowling = 21;
+        private readonly int[] _rolls = new int[MaxRollsInBowling];
+        private int _currentRoll;
+
+        public void Roll(int pins)
+        {
+            _rolls[_currentRoll++] = pins;
+        }
+
+        public int Score()
+        {
+            const int framesInBowling = 10;
+            int score = 0;
+            int frameIndex = 0;
+
+            for (int i = 0; i < framesInBowling; i++)
+            {
+                if (IsStrike(frameIndex))
+                {
+                    score += 10 + BonusForStrike(frameIndex);
+                    frameIndex++;
+                }
+                else if (IsSpare(frameIndex))
+                {
+                    score += 10 + BonusForSpare(frameIndex);
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += SumOfPinsInAFrame(frameIndex);
+                    frameIndex += 2;
+                }
+            }
+
+            return score;
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return SumOfPinsInAFrame(frameIndex) == 10;
+        }
+
+        private bool IsStrike(int frameIndex)
+        {
+            return _rolls[frameIndex] == 10;
+        }
+
+        private int SumOfPinsInAFrame(int frameIndex)
+        {
+            return _rolls[frameIndex] + _rolls[frameIndex + 1];
+        }
+
+        private int BonusForSpare(int frameIndex)
+        {
+            return _rolls[frameIndex + 2];
+        }
+
+        private int BonusForStrike(int frameIndex)
+        {
+            return _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
+        }
+    }
+}
